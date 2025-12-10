1770326e9921d7133bd40603385bd9d9cfd36ccd
Nezalmuhammed <159112277+Nezalmuhammed@users.noreply.github.com>
2025-12-10 23:21:53 +0530

    Refactor icon configuration in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,11 +31,11 @@ export const metadata: Metadata = {
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.png",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+        icons: {
+  icon: "/icon.png",
+  apple: "/icon.png",
+},
+
   },
 }
 
